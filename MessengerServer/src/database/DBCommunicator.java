7ce594362c8fd5f943fbc65195ce448be0473f7e
David RJ <david.rud135@gmail.com>
2019-05-29 21:55:04 +0300

    New DB properties and added javadoc

--- MessengerServer/src/database/DBCommunicator.java
+++ MessengerServer/src/database/DBCommunicator.java
@@ -15,17 +15,20 @@ import messages.User;
 import server.Server;
 
 /**
- *
+ * Class for communication with MySQL DB.
  * @author David Rudenko
  */
 public class DBCommunicator {
   
   private static Connection conn = null;
 
+  /**
+   * Creates new instance of this class.
+   * Creates connection with DB.
+   */
   public DBCommunicator() {
     try {
-//      conn = DBConnector.connectToRemoteDB();
-      conn = DBConnector.connectToLocalDB();
+      conn = DBConnector.connectToRemoteDB();
       System.out.println("Server has connected to Database.");
     } catch (SQLException ex) {
       System.err.println("Server cant connect to Database.");
@@ -34,6 +37,9 @@ public class DBCommunicator {
     launchConnectionRefresher();
   }
   
+  /**
+   * Refreshes MySQL connection every 5 hours.
+   */
   private static void launchConnectionRefresher() {
     long taskInterval = 1000 * 60 * 60 * 5;
     Timer timer = new Timer ();
@@ -54,6 +60,13 @@ public class DBCommunicator {
     timer.schedule(task, taskInterval, taskInterval);
   }
   
+  /**
+   * Adds new user to DB.
+   * @param name - user name, String.
+   * @param email - user email, String.
+   * @param password - user password, String.
+   * @return this operation respond of type {@link auth.AuthRespond}.
+   */
   public static AuthRespond signUpUser(String name, String email, String password) {
     System.out.println(
       String.format("User with email '%s' is trying to Sign Up.", email)
@@ -78,6 +91,12 @@ public class DBCommunicator {
     return authRespond;
   }
   
+  /**
+   * Checks signed in user auth data.
+   * @param email - user email, String.
+   * @param password - user password, String.
+   * @return this operation respond of type {@link auth.AuthRespond}.
+   */
   public static AuthRespond signInUser(String email, String password) {
     System.out.println(
       String.format("User with email '%s' is trying to Sign In.", email)
@@ -112,6 +131,10 @@ public class DBCommunicator {
     return authRespond;
   }
   
+  /**
+   * Gets list of signed up users data from DB.
+   * @return ArrayList of users ({@link messages.User}).
+   */
   public static ArrayList<User> getDBUsersList() {
     final String usersQuery = "SELECT id, name, email FROM users;";
     ArrayList<User> usersList = new ArrayList<>();
--- MessengerServer/src/database/DBConnector.java
+++ MessengerServer/src/database/DBConnector.java
@@ -4,6 +4,10 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+/**
+ * Class for connecting to DB.
+ * @author User
+ */
 public class DBConnector {
   final private static int DB_PORT = 3306;
   final private static String DB_SETTINGS = "?useUnicode=true&characterEncoding=UTF-8&autoReconnect=true";
@@ -14,16 +18,26 @@ public class DBConnector {
   final private static String LOCAL_DB_HOST = "localhost";
   final private static String LOCAL_DB_URL = String.format("jdbc:mysql://%s:%d/%s%s", LOCAL_DB_HOST, DB_PORT, LOCAL_DB_NAME, DB_SETTINGS);  
   
-  final private static String REMOTE_DB_NAME = "sql7292476";
-  final private static String REMOTE_DB_USERNAME = "sql7292476";
-  final private static String REMOTE_DB_PASSWORD = "Ka7utRYIjz";
-  final private static String REMOTE_DB_HOST = "sql7.freemysqlhosting.net";
+  final private static String REMOTE_DB_NAME = "7NiOI4B8W1";
+  final private static String REMOTE_DB_USERNAME = "7NiOI4B8W1";
+  final private static String REMOTE_DB_PASSWORD = "qTO2qu8Joz";
+  final private static String REMOTE_DB_HOST = "remotemysql.com";
   final private static String REMOTE_DB_URL = String.format("jdbc:mysql://%s:%d/%s%s", REMOTE_DB_HOST, DB_PORT, REMOTE_DB_NAME, DB_SETTINGS);  
   
+  /**
+   * Connects to local MySQL DB.
+   * @return successful connection.
+   * @throws SQLException if can't connect.
+   */
   public static Connection connectToLocalDB() throws SQLException {
     return DriverManager.getConnection(LOCAL_DB_URL, LOCAL_DB_USERNAME, LOCAL_DB_PASSWORD);
   }
   
+  /**
+   * Connects to remote MySQL DB.
+   * @return successful connection.
+   * @throws SQLException if can't connect.
+   */
   public static Connection connectToRemoteDB() throws SQLException {
     return DriverManager.getConnection(REMOTE_DB_URL, REMOTE_DB_USERNAME, REMOTE_DB_PASSWORD);
   }
